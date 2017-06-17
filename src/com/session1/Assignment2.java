a6948bf7ddf6abcbafb244762d062eea0cc8fa57
Sudhir.Nayak <Sudhir.Nayak@WLW7-SUDHIRN.INDECOMM.LOCAL>
2017-06-17 16:06:52 +0530

    Session1 Assignment 2

---
+++ src/com/session1/Assignment2.java
@@ -0,0 +1,24 @@
+//Write a program to print the result of the following expressions provided the integer variable a is 20 and b is 10.
+//int b= a-- - --a;
+//int c=a--;
+//int d=a>>2;
+//int e=a&b;
+
+package com.session1;
+
+public class Assignment2 {
+
+public static void main(String[] args) {
+		
+		int a = 20;
+		int b = 10;
+		b= a-- - --a;
+		int c=a--;
+		int d=a>>2;
+		int e=a&b;
+		System.out.println("\nValue of B is "+b +"\nValue of C is "+c+ "\nValue of d is "+d+"\nValue of e is "+e);
+
+	}
+
+
+}
